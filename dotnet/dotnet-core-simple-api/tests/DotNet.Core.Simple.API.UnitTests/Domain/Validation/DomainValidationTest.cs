94f668b40fa4c5d153b93592d286dfe93b32b481
radardovinho <radardovinho@gmail.com>
2025-06-24 22:21:38 -0300

    Unit test to ensure consistency in validations of product entity

---
+++ dotnet/dotnet-core-simple-api/tests/DotNet.Core.Simple.API.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,31 @@
+﻿using DotNet.Core.Simple.API.Domain.Exceptions;
+using DotNet.Core.Simple.API.Domain.Validation;
+
+namespace DotNet.Core.Simple.API.UnitTests.Domain.Validation;
+public class DomainValidationTest
+{
+    [Theory(DisplayName = nameof(ShouldThrowExceptionWhenValueIsNull))]
+    [Trait("Domain", "Validation - DomainValidation")]
+    [InlineData("TestField", null)]
+    public void ShouldThrowExceptionWhenValueIsNull(string fieldName, string? value)
+    {
+        // Act & Assert
+        var action = () => DomainValidation.ValidateNotNull(value, fieldName);
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage($"{fieldName} should not be null.");
+    }
+
+    [Theory(DisplayName = nameof(ShouldThrowExceptionWhenValueIsEmpty))]
+    [Trait("Domain", "Validation - DomainValidation")]
+    [InlineData("TestField", "")]
+    [InlineData("TestField", "    ")]
+    public void ShouldThrowExceptionWhenValueIsEmpty(string fieldName, string? value)
+    {
+        // Act & Assert
+        var action = () => DomainValidation.ValidateNotEmpty(value, fieldName);
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage($"{fieldName} should not be empty.");
+    }
+}
